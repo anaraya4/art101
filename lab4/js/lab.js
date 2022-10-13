d4e0614d49dbd1103cc90caee41bffbb4ba58ebe
anaraya4 <anaraya4@ucsc.edu>
2022-10-12 18:33:22 -0700

    hi

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Aakaash and Gio.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
