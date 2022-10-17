c0dd09fa7fd574359a1cf3c4fe56b258de7702cb
anaraya4 <anaraya4@ucsc.edu>
2022-10-17 01:21:14 -0700

    hui

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -10,3 +10,14 @@
  */
 
 /** jshint {inline configuration here} */
+
+//Print the board befpre each turn
+  //Loop through each ro
+    //Loop through each space
+      //Prnt either Blue or red O, or space
+      //Print || in between
+    //Print newline at the end of each row
+  //see if anyone has 4 in a row, either horizontally, verticall or diagonally
+  //if user has 4 in a row, print "You Win"
+  //if computer has 4 in a row, print "You lose"
+  //If all the tiles are filled and no one has 4 in a row, print "Draw"
