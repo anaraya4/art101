47433b449d5e87bb2aa82709df4cf2d0f84b7610
anaraya4 <anaraya4@ucsc.edu>
2022-10-17 01:13:48 -0700

    hi

---
+++ lab5/js/lab5.js
@@ -0,0 +1,18 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Aakaash.
+// @since  x.x.x
+
+make="Audi";
+model="Q7";
+color="White";
+year=2019;
+age=2022-year;
+
+document.writeln("Make: " + make + "<br>")
+document.writeln("Model: " + model + "<br>")
+document.writeln("Color: " + color + "<br>")
+document.writeln("Year: " + year + "<br>")
+document.writeln("Age: " + age + " years<br>");
